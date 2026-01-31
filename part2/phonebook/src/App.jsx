36393194c332df874f27a3263238054f2dadd19e
sebascad <sebasmesacadiz5@gmail.com>
2026-01-31 20:26:49 +0100

    Finished Exercise 2.15 and refactored it a little

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,126 @@
+import { useState , useEffect} from 'react'
+import personService from './services/persons'
+
+const Filter = ({filter,filterByName}) => <p>filter shown with <input value={filter} onChange={filterByName}/> </p>
+
+const Form = (props) => {
+  return(
+    <form onSubmit={props.addPerson}>
+        <div>
+          <p>name: <input value={props.newName} onChange={props.handlePerson}/></p>
+          <p>number: <input value={props.newNumber} onChange={props.handleNumber}/></p>
+        </div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Persons = ({persons, handleDelete}) => {
+  return(
+    <div>
+      {persons.map((person) => 
+        <p key={person.name}>  {person.name} {person.number} 
+          <button onClick={() => handleDelete(person.id)}>delete</button>
+        </p>)}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState("")
+  const [filter,setFilter] = useState("")
+
+  const personAlreadyExists = (name) => persons.some(person => person.name === name)
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  },[])
+
+  const changeNumber = () => {
+    const change = window.confirm(newName + 
+        " is already added to phonebook, replace the old number with a new one?")
+
+      if(change){
+        const person = persons.find(person => person.name === newName)
+        const updatedPerson = {...person,number: newNumber}
+
+        personService.changeNumber(updatedPerson)
+          .then(response => {
+            setPersons(persons.map(person => person.name === newName ? response.data : person))
+            setNewName("")
+            setNewNumber("")
+          })
+      }
+  }
+
+  const addPerson = () =>{
+    const newPerson = {
+      name: newName,
+      number:newNumber
+    }
+
+    personService
+      .create(newPerson)
+      .then(response =>{
+        setPersons(persons.concat(response.data))
+        setNewName("")
+        setNewNumber("")
+        })
+  }
+
+  const deletePerson = (id) => {
+    const person = persons.find((person) => person.id === id)
+
+    if(window.confirm(`Delete ${person.name} ?`)){
+      personService
+      .deletePerson(id)
+      .then(() =>{
+        setPersons(persons.filter(person => person.id !== id))
+      })
+    }
+  }
+
+  const handleInput = (event) => {
+    event.preventDefault()
+    personAlreadyExists(newName) ? changeNumber() : addPerson()
+  }
+
+  const handlePerson = (event) =>{
+    setNewName(event.target.value)
+  }
+  
+  const handleNumber= (event) =>{
+    setNewNumber(event.target.value)
+  }
+
+  const filterByName = (event) =>{
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} filterByName={filterByName}/>
+
+      <h3>add a new:</h3>
+      <Form addPerson={handleInput} newName={newName} newNumber={newNumber} 
+      handleNumber={handleNumber} handlePerson={handlePerson}/>
+
+      <h2>Numbers</h2>
+      <Persons persons={personsToShow} handleDelete={deletePerson}/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/services/persons.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = "http://localhost:3001/persons/"
+
+const getAll = () =>{
+    return axios.get(baseUrl)
+}
+
+const create = newPerson => {
+    return axios.post(baseUrl,newPerson)
+}
+
+const deletePerson = id => {
+    return axios.delete(baseUrl + id)
+}
+
+const changeNumber = changedperson => {
+    return axios.put(baseUrl + changedperson.id,changedperson)
+}
+
+export default {getAll,create,deletePerson,changeNumber}
