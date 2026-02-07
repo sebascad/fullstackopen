ea2dc9e0ab37163dfb90c97a48b7e601aed3c5dd
sebascad <sebasmesacadiz5@gmail.com>
2026-02-07 21:20:25 +0100

    Changed fecthing from the DB and saving data to it (Exercise 3.14)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,110 +2,69 @@ const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
 const app = express()
+const Person = require("./models/person")
+const PORT = process.env.PORT | 3001
 
 app.use(express.static("dist"))
 app.use(cors())
 app.use(express.json())
 
-morgan.token("body", (req) => JSON.stringify(req.body))
+morgan.token("body", (req) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+)
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
-
-const PORT = process.env.PORT | 3001
-
-  let persons = [
-
-      { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-      },
-      { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-      },
-      { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-      },
-      { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-      }
-  ]
-
-  const nameAlreadyExists = (name) =>{
-    return persons.some((person) => person.name.toLowerCase() === name.toLowerCase())
-  }
-
-  app.get("/api/persons", (request,response) => {
-      response.json(persons)
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
   })
+})
 
-  app.get("/api/persons/:id",(request,response) =>{
-      const id = request.params.id
-      const person = persons.find(person => person.id === id)
-
-      if(person){
-          response.json(person)
-      }else{
-          response.status(404).end()
-      }
+app.get("/api/persons/:id", (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
   })
+})
 
-  app.get("/info", (request, response) => {
-      response.send(`
+app.get("/info", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.send(`
         <div>
           <p>Phonebook has info for ${persons.length} people</p>
           <p>${new Date()}</p>
         </div>
       `)
   })
+})
 
-  app.delete("/api/persons/:id", (request,response) =>{
-      const id = request.params.id
-      persons = persons.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id
+  persons = persons.filter((person) => person.id !== id)
 
-      response.status(204).end()
-  })
+  response.status(204).end()
+})
 
-  const generateId = () =>{
-    return String(Math.floor(Math.random() * 100000))
+app.post("/api/persons", (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    })
   }
 
-  app.post("/api/persons",(request,response) => {
-      const body = request.body
-      
-      if(!body.name || !body.number){
-        return response.status(400).json({
-          error: "content missing"
-        })
-      }
-      
-      console.log(body.name)
-
-      if(nameAlreadyExists(body.name)){
-        return response.status(400).json({
-          error: "name must be unique"
-        }) 
-      }
-
-
-      const person = {
-        id: generateId(),
-        name : body.name,
-        number : body.number,
-      }
-
-      persons = persons.concat(person)
-
-      morgan.token()
-      return response.json(person)
-  } )
-
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  const person = new Person({
+    name: body.name,
+    number: body.number,
   })
+
+  morgan.token()
+  
+  person.save().then(savedPerson =>{
+    response.json(savedPerson)
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
