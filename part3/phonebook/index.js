a3f5c049b29b5aa6a80984b747fd337d3e8d6a93
sebascad <sebasmesacadiz5@gmail.com>
2026-02-08 21:12:21 +0100

    Added middleware for handling errors and finished optional exercises (3.18)

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -20,10 +20,15 @@ app.get("/api/persons", (request, response) => {
   })
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response,next) => {
   Person.findById(request.params.id).then((person) => {
-    response.json(person);
+    if(person){
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
@@ -37,14 +42,28 @@ app.get("/info", (request, response) => {
   })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response,next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result =>{
       response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.put("/api/persons/:id",(request,response) =>{
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id,person)
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+})
+
+app.post("/api/persons", (request, response,next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -63,7 +82,26 @@ app.post("/api/persons", (request, response) => {
   person.save().then(savedPerson =>{
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+const unknownEndpoint = (request,response) =>{
+  response.status(400).send({error: "unknown endpoint"})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error,request,response,next) =>{
+  console.log(error.message)
+
+  if(error.name === "CastError"){
+    return response.status(400).send({error: "malformatted id"})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
