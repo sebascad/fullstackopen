b8fc99c131bb260e06ac2628f08683a4f7f28e53
sebascad <sebasmesacadiz5@gmail.com>
2026-02-12 16:38:57 +0100

    Refactored the app and added tests, finished exercise 4.12

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -6,12 +6,22 @@ blogRouter.get('/', async (request, response) => {
   return response.json(blogs)
 })
 
+blogRouter.get('/:id', async (request,response) => {
+  const blog = await Blog.findById(request.params.id)
+  return response.json(blog)
+})
+
 blogRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
   if(!blog.url || !blog.title) return response.status(400).end()
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
+})
+
+blogRouter.delete('/:id', async(request,response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogRouter
