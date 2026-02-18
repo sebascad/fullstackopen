0e195c6310ff7b1d6ce543c7d2fbf550604e1275
sebascad <sebasmesacadiz5@gmail.com>
2026-02-18 22:11:59 +0100

    Integrated token authentication

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/error_handler')
+const errorHandler = require('./utils/error_handler')
+const middleware = require('./utils/middleware')
 
 const blogRouter = require('./controllers/blogs')
 const userRouter = require('./controllers/users')
@@ -19,13 +20,14 @@ mongoose
   })
 
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs',blogRouter)
 app.use('/api/users',userRouter)
 app.use('/api/login',loginRouter)
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+app.use(errorHandler.unknownEndpoint)
+app.use(errorHandler.errorHandler)
 
 
 module.exports = app
--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,16 +1,8 @@
 const blogRouter = require('express').Router()
 const jwt = require('jsonwebtoken')
-const logger = require('../utils/logger')
+
 const Blog = require('../models/blog')
 const User = require('../models/user')
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')){
-    return authorization.replace('Bearer ',' ')
-  }
-  return null
-}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -34,10 +26,8 @@ blogRouter.get('/:id', async (request,response,next) => {
 blogRouter.post('/', async (request, response) => {
   const body = request.body
   if(!body.url || !body.title) return response.status(400).end()
-  logger.info(getTokenFrom(request))
-  logger.info(getTokenFrom(process.env.SECRET))
-  
-  const decodedToken = jwt.verify(getTokenFrom(request),process.env.SECRET)
+
+  const decodedToken = jwt.verify(request.token ,process.env.SECRET)
   if(!decodedToken.id){
     return response.status(401).json({ error: 'token invalid' })
   }
